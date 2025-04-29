55eb8e232deacc8e7f14d2da9e76dfabab506023
Zeerooo-dev <jeffirie12@gmail.com>
2025-04-29 13:23:08 +0800

    first commit

---
+++ app/src/main/java/com/example/try2/main.java
@@ -0,0 +1,21 @@
+package com.example.try2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.main);
+
+
+
+
+    }
+}
